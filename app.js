735add7f7360c6dbb20876f1a95636e43cf2ffa4
soosap <prasath.soosaithasan@gmail.com>
2015-12-13 19:07:13 +0100

    Add first app.use() statement
    app.use() is Express's middleware and will run prior any express route will be executed.
    
    app.use(express.static('public'));
    
    This will make Express serve the public folder as static.

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 // node is just a runtime environment that executes js code on the server
-
 var express = require('express');
 var app = express();
 
 var port = 5000;
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+		res.send('Hello World');
+});
+
 app.listen(port, function (err) {
 		console.log('Running server on port ' + port);
 });
